6347687540962a6e0074c7b8c873e6ab4be494fb
Oleg <rbghero@gmail.com>
2023-01-26 14:04:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 10000;
+        int rub = 20;
+        int bonus = amount / rub;
+        System.out.println("Итоговые бонусные мили равны " + bonus);
+    }
+}
